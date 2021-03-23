441b1fdcba857c4deea540aa58acf36109dbeadc
Andrey Erusaev <andrey.erusaev@ix.co>
2021-03-23 23:47:40 +0300

    Clear SSMLDecoder

--- AimyboxCore/TextToSpeech/SSMLDecoder.swift
+++ AimyboxCore/TextToSpeech/SSMLDecoder.swift
@@ -8,39 +8,19 @@
 
 import Foundation
 
-public extension Array where Element == AimyboxSpeech {
-    
-    var unwrapSSML: [Element] {
-        
-        var unwrapped = [Element]()
+final class SSMLDecoder: NSObject, XMLParserDelegate {
 
-        forEach { elem in
-            if let textSpeech = elem as? TextSpeech {
-                SSMLDecoder().decode(textSpeech.text).forEach { decodedSpeech in
-                    unwrapped.append(decodedSpeech)
-                }
-            } else {
-                unwrapped.append(elem)
-            }
-        }
-        
-        return unwrapped
-    }
-}
+    private let parserGroup = DispatchGroup()
 
+    private var audioTagUrl: URL?
 
-public final class SSMLDecoder: NSObject, XMLParserDelegate {
+    private var pTagText: String?
 
-    public override init() {
-        speeches = []
-        parserGroup = DispatchGroup()
-        p_tag_text = ""
-        super.init()
-    }
-    
-    public func decode(_ string: String) -> [AimyboxSpeech] {
+    private var speeches: [AimyboxSpeech] = []
+
+    func decode(_ string: String) -> [AimyboxSpeech] {
         let wrapped = "<p>\(string)</p>"
-        
+
         if let data = wrapped.data(using: .utf8) {
             return decode(data)
         } else {
@@ -48,72 +28,73 @@ public final class SSMLDecoder: NSObject, XMLParserDelegate {
         }
     }
     
-    private let parserGroup: DispatchGroup
+    func parser(
+        _ parser: XMLParser,
+        didStartElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName qName: String?,
+        attributes attributeDict: [String : String] = [:]
+    ) {
+        if elementName == pTag {
+            pTagText = nil
+        } else if elementName == audioTag {
+            if let src = attributeDict["src"], let url = URL(string: src) {
+                audioTagUrl = url
+            } else {
+                audioTagUrl = nil
+            }
+        }
+    }
     
-    private let p_tag: String = "p"
+    func parser(
+        _ parser: XMLParser,
+        didEndElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName qName: String?
+    ) {
+        if elementName == pTag {
+            guard let pTagText = pTagText else {
+                return
+            }
+            speeches.append(TextSpeech(text: pTagText.trimmingCharacters(in: .whitespaces)))
+            self.pTagText = nil
+        } else if elementName == audioTag {
+            guard let audioTagUrl = audioTagUrl else {
+                return
+            }
+            speeches.append(AudioSpeech(audioURL: audioTagUrl))
+            self.audioTagUrl = nil
+        }
+    }
+
+    func parserDidEndDocument(_ parser: XMLParser) {
+        parserGroup.leave()
+    }
     
-    private var p_tag_text: String?
-    
-    private let audio_tag: String = "audio"
-    
-    private var audio_tag_url: URL?
-    
-    private var speeches: [AimyboxSpeech]
-    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if pTagText == nil {
+            pTagText = string
+        } else {
+            pTagText?.append(string)
+        }
+    }
+
     private func decode(_ data: Data) -> [AimyboxSpeech] {
         let parser = XMLParser(data: data)
         parser.delegate = self
         parser.shouldProcessNamespaces = false
         parser.shouldReportNamespacePrefixes = false
         parser.shouldResolveExternalEntities = false
-        
+
         parserGroup.enter()
         parser.parse()
         parserGroup.wait()
-        
+
         return speeches
     }
-    
-    public func parserDidEndDocument(_ parser: XMLParser) {
-        parserGroup.leave()
-    }
-    
-    public func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        if elementName == p_tag {
-            p_tag_text = nil
-        } else if elementName == audio_tag {
-            if let src = attributeDict["src"], let url = URL(string: src) {
-                audio_tag_url = url
-            } else {
-                audio_tag_url = nil
-            }
-        }
-    }
-    
-    public func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        if elementName == p_tag {
-            guard let _p_tag_text = p_tag_text else { return }
 
-            speeches.append(
-                TextSpeech(text: _p_tag_text.trimmingCharacters(in: .whitespaces))
-            )
-            p_tag_text = nil
-        } else if elementName == audio_tag {
-            guard let _audio_tag_url = audio_tag_url else { return }
-            
-            speeches.append(
-                AudioSpeech(audioURL: _audio_tag_url)
-            )
-            audio_tag_url = nil
-        }
-    }
-    
-    public func parser(_ parser: XMLParser, foundCharacters string: String) {
-        if p_tag_text == nil {
-            p_tag_text = string
-        } else {
-            p_tag_text?.append(string)
-        }
-
-    }
 }
+
+private let audioTag: String = "audio"
+
+private let pTag: String = "p"
--- Components/YandexSpeechKit/Sources/YandexSynthesisAPI/YandexTextToSpeech.swift
+++ Components/YandexSpeechKit/Sources/YandexSynthesisAPI/YandexTextToSpeech.swift
@@ -196,3 +196,21 @@ public class YandexTextToSpeech: AimyboxComponent, TextToSpeech {
         }
     }
 }
+
+private extension Array where Element == AimyboxSpeech {
+
+    var unwrapSSML: [Element] {
+        var unwrapped = [Element]()
+        forEach { elem in
+            if let textSpeech = elem as? TextSpeech {
+                SSMLDecoder().decode(textSpeech.text).forEach {
+                    unwrapped.append($0)
+                }
+            } else {
+                unwrapped.append(elem)
+            }
+        }
+        return unwrapped
+    }
+}
+
